498d004bc73f73183ade7fdc4a36fad8093e6dde
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-12-29 11:14:57 +0330

    Add all subfolders to compiling

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("styles/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("styles/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchStyles() {
-  return watch("styles/*.scss", buildStyles);
+  return watch("styles/**/*.scss", buildStyles);
 }
 
 exports.default = series(buildStyles, watchStyles);
