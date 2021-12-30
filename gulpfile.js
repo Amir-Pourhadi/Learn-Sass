8f604c02bbbfea6fca1e7dd1d401f68046758aa9
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-12-30 13:23:37 +0330

    Add purgecss gulp commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("styles/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("styles/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
 function watchStyles() {
-  return watch("styles/**/*.scss", buildStyles);
+  return watch(["styles/**/*.scss", "*.html"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchStyles);
